f7e07ed70c64e686bbb711fd07c9f4962c507bc3
Du <DDU@sba.gov>
2019-06-25 00:31:14 -0400

    create validations

--- app/models/glsuser.rb
+++ app/models/glsuser.rb
@@ -1,5 +1,5 @@
 #connection between controller and database
 class Glsuser < ActiveRecord::Base
-  validates :username, presence: true
-  validates :password, presence: true
+  validates :username, presence: true, length: {minumum: 3,maximum: 50}
+  validates :password, presence: true, length: {minimum: 8, maximum: 20}
 end
