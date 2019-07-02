361d0b0a3ba20c195cbe41b1b07d3964ce702720
DDU <DDU@CIO-EB84305SW.sba.gov>
2019-07-02 16:25:10 -0400

    unit testing for category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
-	  has_many :notes, dependent: :nullify
+	  has_many :notes, dependent: :nullify #when the category is deleted, the associated table column is set to null
+	  validates :name, presence: true, length: {minimum: 3, maximum: 20}
+	  #validates_uniqueness_of :name
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -10,6 +10,27 @@ class CategoryTest < ActiveSupport::TestCase
   test "category should be valid" do
      assert @category.valid?
   end
+  
+  test "name should be present" do
+      @category.name = " "
+      assert_not @category.valid?
+  end
+  
+  # test "name should be unique" do
+   # @category.save
+   # @category_copy = Category.new(name: "sports")
+   # assert_not @category_copy.valid?
+  # end
+  
+  test "name should not be too long" do
+    @category.name = "a"*26
+    assert_not @category.valid?
+  end
+  
+  test "name should not be too short" do
+      @category.name = "aa"
+      assert_not @category.valid?
+    end
 end
 
 #run command:  rails test
